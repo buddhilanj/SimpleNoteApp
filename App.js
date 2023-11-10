f2247b455a7b094fedb4289b9210a0fa84387c6d
buddhilanj <buddhilanj@gmail.com>
2023-11-10 14:26:45 +1100

    Redux Persist and Clean up

--- App.js
+++ App.js
@@ -1,17 +1,15 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text } from "react-native";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-import { store } from "./src/store/store";
-// import { store, persistor } from "./src/store/persistedStore";
+import { store, persistor } from "./src/store/persistedStore";
 import AppNavigation from "./src/navigation/RootNavigation";
 
 export default function App() {
   return (
     <Provider store={store}>
-      {/* <PersistGate loading={<Text>Loading...</Text>} persistor={persistor}> */}
-      <AppNavigation />
-      {/* </PersistGate> */}
+      <PersistGate loading={<Text>Loading...</Text>} persistor={persistor}>
+        <AppNavigation />
+      </PersistGate>
     </Provider>
   );
 }
